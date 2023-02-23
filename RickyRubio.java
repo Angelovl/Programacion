675e7023260429ef7988982476f54fac8f473851
Angelo Visentin <Angelovisentin8@gmail.com>
2023-02-23 17:01:34 +0100

    Añadido mi primer Commit

---
+++ RickyRubio.java
@@ -0,0 +1,42 @@
+package programacion;
+public class RickyRubio {
+public static void main(String[] args) {
+    
+    
+     //Datos Personales
+     String nombre= "Ricky";
+     String apellidos= "Rubio";
+     int edad= 32;
+     int altura= 188;
+     
+     //Datos del Equipo
+     int dorsal= 13;
+     String equipo= "Cavaliers";
+     
+     //Temporada 21-22
+     double puntosPartido= 13.1;
+     double rebPartidos=4.1;
+     double asistPartidos= 6.6;
+     boolean lesionado=true;
+     
+     //Datos por pantalla
+     System.out.println("------Datos Personales------");
+     System.out.println("Nombre: "+nombre+" "+apellidos);
+     System.out.println("Edad: "+edad+" años");
+     System.out.println("Altura: "+altura+"cm");
+     System.out.println("");
+     System.out.println("------Datos del Equipo------");
+     System.out.println("Número de Dorsal: "+dorsal);
+     System.out.println("Nombre del Equipo: "+equipo);
+     System.out.println("");
+     System.out.println("------Temporada 2021-2022------");
+     System.out.println("Media Puntos por Partido: "+puntosPartido);
+     System.out.println("Media de Rebotes por Partido: "+rebPartidos);
+     System.out.println("Media de Asistencias por Partido: "+asistPartidos);
+     System.out.println("Esta Lesionado? "+lesionado);
+     
+     
+        
+    }
+    
+}
